9d45f4c0c6015a33c802d8c370e71c64c35d9492
GuoJikun <guojikun486546@163.com>
2023-02-15 14:48:13 +0800

    ✨ feat: 优化菜单

--- src/.vuepress/config/sidebar.js
+++ src/.vuepress/config/sidebar.js
@@ -6,35 +6,35 @@ export default {
       children: [
         {
           text: "Filter 滤镜",
-          link: "/basic/filter.md",
+          link: "/basic/filter",
         },
         {
           text: "Cursor 鼠标样式",
-          link: "/basic/cursor.md",
+          link: "/basic/cursor",
         },
         {
           text: "Intersection Observer",
-          link: "/basic/intersection-observer.md",
+          link: "/basic/intersection-observer",
         },
         {
           text: "Canvas",
-          link: "/basic/canvas.md",
+          link: "/basic/canvas",
         },
         {
           text: "WebComponent",
-          link: "/basic/web-component.md",
+          link: "/basic/web-component",
         },
         {
           text: "Scss之extend和mixin",
-          link: "/basic/p2021091401.md",
+          link: "/basic/p2021091401",
         },
         {
           text: "Css 之 contain",
-          link: "/basic/contain.md",
+          link: "/basic/contain",
         },
         {
           text: "自定义滚动条样式",
-          link: "/basic/scrollbar.md",
+          link: "/basic/scrollbar",
         },
       ],
     },
@@ -44,33 +44,33 @@ export default {
       children: [
         {
           text: "Cookie 缓存",
-          link: "/basic/js/cookie.md",
+          link: "/basic/js/cookie",
         },
         {
           text: "Decorator 装饰器",
-          link: "/basic/js/decorator.md",
+          link: "/basic/js/decorator",
         },
         {
           text: "编解码",
-          link: "/basic/js/encode.md",
+          link: "/basic/js/encode",
         },
         {
           text: "Utils 工具函数",
-          link: "/basic/js/utils.md",
+          link: "/basic/js/utils",
         },
         {
           text: "浏览器多窗口通讯",
-          link: "/basic/js/multi-window-communication.md",
+          link: "/basic/js/multi-window-communication",
         },
         {
           text: "MutationObserver API",
-          link: "/basic/js/mutation-observer.md",
+          link: "/basic/js/mutation-observer",
         },
         {
           text: "手写new,bind,call,apply",
-          link: "/basic/js/p2021091402.md",
+          link: "/basic/js/p2021091402",
         },
-        { text: "获取滚动条宽度", link: "/basic/scrollbar-width.md" },
+        { text: "获取滚动条宽度", link: "/basic/scrollbar-width" },
       ],
     },
     {
@@ -79,31 +79,31 @@ export default {
       children: [
         {
           text: "ECMA 2022",
-          link: "/basic/ecma/2022.md",
+          link: "/basic/ecma/2022",
         },
         {
           text: "ECMA 2021",
-          link: "/basic/ecma/2021.md",
+          link: "/basic/ecma/2021",
         },
         {
           text: "ECMA 2020",
-          link: "/basic/ecma/2020.md",
+          link: "/basic/ecma/2020",
         },
         {
           text: "ECMA 2019",
-          link: "/basic/ecma/2019.md",
+          link: "/basic/ecma/2019",
         },
         {
           text: "ECMA 2018",
-          link: "/basic/ecma/2018.md",
+          link: "/basic/ecma/2018",
         },
         {
           text: "ECMA 2017",
-          link: "/basic/ecma/2017.md",
+          link: "/basic/ecma/2017",
         },
         {
           text: "ECMA 2016",
-          link: "/basic/ecma/2016.md",
+          link: "/basic/ecma/2016",
         },
       ],
     },
@@ -114,7 +114,7 @@ export default {
       children: [
         {
           text: "Copy 复制",
-          link: "/vue/directives/copy.md",
+          link: "/vue/directives/copy",
         },
       ],
     },
@@ -123,11 +123,11 @@ export default {
       children: [
         {
           text: "ContentMenu 右键菜单",
-          link: "/vue/component/content-menu.md",
+          link: "/vue/component/content-menu",
         },
         {
           text: "Barcode 条形码",
-          link: "/vue/component/barcode.md",
+          link: "/vue/component/barcode",
         },
       ],
     },
@@ -136,11 +136,11 @@ export default {
       children: [
         {
           text: "env 配置文件",
-          link: "/vue/env.md",
+          link: "/vue/env",
         },
         {
           text: "vueCli项目添加vite支持",
-          link: "/vue/p202109101.md",
+          link: "/vue/p202109101",
         },
       ],
     },
@@ -152,27 +152,27 @@ export default {
       children: [
         {
           text: "List 列表",
-          link: "/data-structure/list.md",
+          link: "/data-structure/list",
         },
         {
           text: "Queue 队列",
-          link: "/data-structure/queue.md",
+          link: "/data-structure/queue",
         },
         {
           text: "Stack 栈",
-          link: "/data-structure/stack.md",
+          link: "/data-structure/stack",
         },
         {
           text: "Set 集合",
-          link: "/data-structure/set.md",
+          link: "/data-structure/set",
         },
         {
           text: "Linked List 链表",
-          link: "/data-structure/linked-list.md",
+          link: "/data-structure/linked-list",
         },
         {
           text: "Tree 树",
-          link: "/data-structure/binary-search-tree.md",
+          link: "/data-structure/binary-search-tree",
         },
       ],
     },
@@ -181,11 +181,11 @@ export default {
       children: [
         {
           text: "Sort 排序",
-          link: "/data-structure/sort.md",
+          link: "/data-structure/sort",
         },
         {
           text: "Tween 缓动动画",
-          link: "/data-structure/tween.md",
+          link: "/data-structure/tween",
         },
       ],
     },
@@ -196,11 +196,11 @@ export default {
       children: [
         {
           text: "阴影生成器",
-          link: "/tools/shadow.md",
+          link: "/tools/shadow",
         },
         {
           text: "文件转Base64",
-          link: "/tools/file-to-base64.md",
+          link: "/tools/file-to-base64",
         },
       ],
     },
@@ -211,7 +211,7 @@ export default {
       children: [
         {
           text: "watermark 水印",
-          link: "/docs/watermark-vue.md",
+          link: "/docs/watermark-vue",
         },
       ],
     },
@@ -222,35 +222,35 @@ export default {
       children: [
         {
           text: "win11安装 wslg",
-          link: "/other/wsl.md",
+          link: "/other/wsl",
         },
         {
           text: "搭建私有Npm仓库",
-          link: "/other/private-npm-repository.md",
+          link: "/other/private-npm-repository",
         },
         {
           text: "win10 激活",
-          link: "/other/activation-win10.md",
+          link: "/other/activation-win10",
         },
         {
           text: "Scoop 包管理器",
-          link: "/other/scoop.md",
+          link: "/other/scoop",
         },
         {
           text: "linux 禁止指定软件升级",
-          link: "/other/disabled-upgrade.md",
+          link: "/other/disabled-upgrade",
         },
         {
           text: "Markdown 基础语法",
-          link: "/other/md.md",
+          link: "/other/md",
         },
         {
           text: "Windows终端代理",
-          link: "/other/terminal-proxy.md",
+          link: "/other/terminal-proxy",
         },
         {
           text: "更改wsl的安装位置",
-          link: "/other/move-wsl-install-dir.md",
+          link: "/other/move-wsl-install-dir",
         },
       ],
     },
