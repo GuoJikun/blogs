fb12fdee07877c01d05d50995f2f1bb7c9d1427c
GuoJikun <guojikun486546@163.com>
2023-05-17 15:45:03 +0800

    ✨ feat: 优化vitepress配置

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -6,20 +6,16 @@ export default defineConfig({
     base: "/",
     lang: "zh-CN",
     title: "前端小栈",
-    description: "这是我的第一个 VuePress 站点",
     outDir: "../dist",
     lastUpdated: true,
     themeConfig: {
         home: "/index.md",
         logo: "https://vuejs.org/images/logo.png",
-        repo: "GuoJikun/blog-vuepress",
-        docsRepo: "https://github.com/GuoJikun/blog-vuepress",
-        docsBranch: "main",
-        docsDir: "src",
-        editLinkPattern: ":repo/edit/:branch/:path",
-        editLink: true,
-        editLinkText: "在 GitHub 上编辑此页",
-        lastUpdated: true,
+        editLink: {
+            text: "在 GitHub 上编辑此页",
+            pattern:
+                "https://github.com/GuoJikun/blog-vuepress/edit/main/src/:path",
+        },
         lastUpdatedText: "最后更新时间",
         outline: [2, 3],
         outlineTitle: "目录",
@@ -33,7 +29,7 @@ export default defineConfig({
         sidebar: sidebar,
         footer: {
             message: "MIT Licensed",
-            copyright: "Copyright © 2022-present GuoJiKun",
+            copyright: "Copyright © 2023-present GuoJiKun",
         },
         docFooter: {
             prev: "上一页",
