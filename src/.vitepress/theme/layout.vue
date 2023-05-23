de525a259ad7f7ee3eccc3d87e58ea166ba16929
GuoJikun <guojikun486546@163.com>
2023-05-23 16:46:59 +0800

    ✨ feat: 更新giscus配置

--- src/.vitepress/theme/layout.vue
+++ src/.vitepress/theme/layout.vue
@@ -1,32 +1,32 @@
 <script setup>
-import DefaultTheme from "vitepress/theme";
-import VGiscus from "@giscus/vue";
+    import DefaultTheme from "vitepress/theme";
+    import VGiscus from "@giscus/vue";
 
-const { Layout } = DefaultTheme;
+    const { Layout } = DefaultTheme;
 </script>
 
 <template>
-  <Layout>
-    <template #doc-after>
-      <div style="padding-top: 24px">
-        <v-giscus
-          id="comments"
-          repo="guojikun/blog-vuepress"
-          repo-id="MDEwOlJlcG9zaXRvcnkzOTUyNzM1NDg="
-          category="评论"
-          category-id="DIC_kwDOF49lTM4CV3Gn"
-          mapping="url"
-          strict="1"
-          reactions-enabled="1"
-          emit-metadata="0"
-          input-position="top"
-          theme="preferred_color_scheme"
-          lang="zh-CN"
-          loading="lazy"
-          crossorigin="anonymous"
-          async
-        ></v-giscus>
-      </div>
-    </template>
-  </Layout>
+    <Layout>
+        <template #doc-after>
+            <div style="padding-top: 24px">
+                <v-giscus
+                    id="comments"
+                    repo="guojikun/blog-vuepress"
+                    repoId="MDEwOlJlcG9zaXRvcnkzOTUyNzM1NDg="
+                    category="评论"
+                    categoryId="DIC_kwDOF49lTM4CV3Gn"
+                    mapping="url"
+                    strict="1"
+                    reactionsEnabled="1"
+                    emitMetadata="0"
+                    inputPosition="top"
+                    theme="preferred_color_scheme"
+                    lang="zh-CN"
+                    loading="lazy"
+                    crossorigin="anonymous"
+                    async
+                ></v-giscus>
+            </div>
+        </template>
+    </Layout>
 </template>
